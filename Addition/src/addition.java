f4429e494a4f08020d7080fadf4ff305900d8513
HP <HP@HPNB>
2021-07-07 01:14:52 +0530

    Final Execution

---
+++ Addition/src/addition.java
@@ -0,0 +1,16 @@
+
+public class addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter two integer numbers ");
+		int first = 100 ;
+		int second = 200;
+		System.out.println(first + " " +second );
+		int sum = first + second;
+		System.out.println("The Sum of two numbers is" +sum);
+		
+
+	}
+
+}
