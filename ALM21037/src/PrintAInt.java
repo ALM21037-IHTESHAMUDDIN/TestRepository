203a59f663fe5c7c3aeb42fcde51eb440ec9efc6
HP <HP@HPNB>
2021-07-08 00:10:26 +0530

    Resources leak for reader

--- ALM21037/src/PrintAInt.java
+++ ALM21037/src/PrintAInt.java
@@ -1,9 +1,12 @@
-
+import java.util.Scanner;
 public class PrintAInt {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	}
-
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Enter a number");
+		int number =reader.nextInt();
+		System.out.println("You Entered " + number);
+		
 }
+	}
