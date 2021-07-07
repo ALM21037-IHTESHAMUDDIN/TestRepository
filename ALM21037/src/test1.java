8adf3c02b931f74844889cecc868bb4f19fd78b3
HP <HP@HPNB>
2021-07-08 00:24:44 +0530

    test

---
+++ ALM21037/src/test1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner reader = new Scanner(System.in);
+		System.out.print("Enter a number: ");
+		// nextInt() reads the next integer from the keyboard
+		int number = reader.nextInt();
+		// println() prints the following line to the output screen
+		System.out.println("You entered: " + number);
+
+	}
+
+}
