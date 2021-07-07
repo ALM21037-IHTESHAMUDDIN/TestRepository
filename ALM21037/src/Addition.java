c5e15453bd815ba0971401098f09fc897f184d0c
HP <HP@HPNB>
2021-07-07 23:52:39 +0530

    final commit

---
+++ ALM21037/src/Addition.java
@@ -0,0 +1,15 @@
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.println("Enter two number");
+         int first = 100;
+         int second = 200;
+         System.out.println( "You have Entered " + first + " " + second);
+         int sum = first + second;
+         System.out.println("The Sum is " + sum);
+         
+	}
+
+}
